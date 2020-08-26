893fb36f7cf85a361e9e705f1886b219898d3f87
Matthew <mreverssci@gmail.com>
2020-08-26 07:39:14 -0500

    feat: move re-exported module

--- projects/webview_native_api/src/lib.rs
+++ projects/webview_native_api/src/lib.rs
@@ -1,4 +1,3 @@
-pub mod command_types;
 mod native_apis;
 mod native_handler;
 
@@ -10,5 +9,6 @@ pub fn provide_native_api<'a>(webview: &mut webview_official::Webview<'a>) {
     webview.bind("__native__", create_native_api(w));
 }
 
+pub mod command_types;
 pub use native_handler::create_handler;
 pub use native_handler::CommandResult;
